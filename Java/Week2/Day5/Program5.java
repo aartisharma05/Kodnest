410b3f06c69642babc85abd4cf6107cf5af2c830
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-06-21 12:53:22 +0530

    Create Program5.java

---
+++ Java/Week2/Day5/Program5.java
@@ -0,0 +1,15 @@
+class Program5 {
+    public static void main(String[] args) {
+    byte numberOfCountries = 110;
+    short availableTitles = 15000;
+    int totalSubscribers = 200000000;
+    long streamingMinutes = 10000000000000L;
+    
+    System.out.println("Netflix is available in " +numberOfCountries + " countries.");
+    System.out.println("Netflix offers " +availableTitles + " unique titles worldwide.");
+    System.out.println("Netflix has " +totalSubscribers + " subscribers globally.");
+    System.out.println("Total streaming minutes on Netflix last year :" +streamingMinutes+".");
+    }
+    
+    
+}

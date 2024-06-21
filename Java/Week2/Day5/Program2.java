b6edf4d3fbbf8cf1e6632c7bcaee2f104ea52297
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-06-21 12:33:08 +0530

    Update Program2.java

--- Java/Week2/Day5/Program2.java
+++ Java/Week2/Day5/Program2.java
@@ -2,6 +2,6 @@ class Program2 {
     public static void main(String[] args) {
     
     byte numberOfCountries = 110;
-    System.out.println(numberOfCountries);
+    System.out.println("Netflix is available in " +numberOfCountries + " countries.");
     }
 }

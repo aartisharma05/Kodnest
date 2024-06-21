2365101119eb505d7c5bfc720cfe550d053d3ddf
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-06-21 12:30:09 +0530

    Create Program1.java

---
+++ Java/Week2/Day5/Program1.java
@@ -0,0 +1,11 @@
+class HelloWorld {
+    public static void main(String[] args) {
+      int c = 045;
+      int d = 0X45;
+      int e = 0b01001010;
+      System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+  
+    }
+}

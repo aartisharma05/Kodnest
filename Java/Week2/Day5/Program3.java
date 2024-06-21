8242cb081fa2cf8ebb3a6a41ec996fae3c1db13a
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-06-21 12:42:25 +0530

    Create Program3.java

---
+++ Java/Week2/Day5/Program3.java
@@ -0,0 +1,8 @@
+class Program3 {
+    public static void main(String[] args) {
+    byte numberOfCountries = 110;
+    short availableTitles = 15000;
+    System.out.println("Netflix is available in " +numberOfCountries + " countries.");
+    System.out.println("Netflix offers " +availableTitles + " unique titles worldwide.");
+    }
+}

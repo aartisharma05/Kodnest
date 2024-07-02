ad5bedddb0f91f15b1fb0e3bf040aed0bf084e5a
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-07-02 11:14:27 +0530

    Create infiniteForLoop.java

---
+++ Java/Week8/Day1/infiniteForLoop.java
@@ -0,0 +1,14 @@
+package loops;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for( ;  ; ) {
+			System.out.println("Infinite for loop");
+		}
+
+	}
+
+}

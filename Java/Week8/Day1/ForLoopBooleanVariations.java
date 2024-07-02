1f0755d7494011b7d631235357a92b92d030d4c8
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-07-02 12:11:40 +0530

    Create ForLoopBooleanVariations.java

---
+++ Java/Week8/Day1/ForLoopBooleanVariations.java
@@ -0,0 +1,31 @@
+package loops;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(boolean a = true; a ==true ; a = false) {
+			System.out.println(a);
+		}
+		
+		System.out.println();
+		for(boolean a = true; a ==false ; a = true) {
+			System.out.println(a);
+		}
+		System.out.println();
+		
+		for(boolean a = false; false ; a = true) {
+			System.out.println(a);
+		}
+		
+		System.out.println();
+		
+		for(boolean a = false; true ; a = false) {
+			System.out.println(a);
+		}
+		
+       
+	}
+
+}

c6b4e01fcfeda82abced865ac4649a3e662a2800
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-07-02 12:24:13 +0530

    Create NestedForLoop.java

---
+++ Java/Week8/Day2/NestedForLoop.java
@@ -0,0 +1,17 @@
+package loops;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	  for(int i =1 ; i<=3; i++) {
+		  for(int j =1;j<=2;j++) {
+			  System.out.println("i:"+ i + " j:"+ j);
+		  }
+		  System.out.println();
+	  }
+	
+	}
+
+}

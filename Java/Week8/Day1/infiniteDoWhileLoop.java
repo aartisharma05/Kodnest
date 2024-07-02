33f4989bafc17678a66786d0a0a13ca6c59308b5
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-07-02 11:34:18 +0530

    Create infiniteDoWhileLoop.java

---
+++ Java/Week8/Day1/infiniteDoWhileLoop.java
@@ -0,0 +1,17 @@
+package loops;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		do {
+			System.out.println("infinite while loop");
+		}
+		while(true);
+			
+		
+       
+	}
+
+}

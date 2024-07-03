aebceb216bc2053c0502489cfd48c1f8e190c7ae
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-07-03 11:35:57 +0530

    Create printingArrays.java

---
+++ Java/JavaPhase2/Arrays/printingArrays.java
@@ -0,0 +1,24 @@
+package arrays;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      float []arr = new float[7];
+      
+      arr[0] = 81.17f;
+      arr[1] = 82.67f;
+      arr[2] = 83.97f;
+      arr[3] = 84.57f;
+      arr[4] = 71.19f;
+      arr[5] = 61.27f;
+      arr[6] = 91.17f;
+      
+      
+      for(int i =0;i<arr.length;i++) {
+    	  System.out.println(arr[i]);
+      }
+      
+	}
+
+}

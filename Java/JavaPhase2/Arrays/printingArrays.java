795a423c072730ae662b2afd8a399480e53298a2
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-07-03 11:37:28 +0530

    Update printingArrays.java

--- Java/JavaPhase2/Arrays/printingArrays.java
+++ Java/JavaPhase2/Arrays/printingArrays.java
@@ -4,7 +4,11 @@ public class Arrays {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      float []arr = new float[7];
+      float []arr = new float[7]; //Creating the array
+
+
+
+		//Storing the data in array
       
       arr[0] = 81.17f;
       arr[1] = 82.67f;
@@ -14,7 +18,8 @@ public class Arrays {
       arr[5] = 61.27f;
       arr[6] = 91.17f;
       
-      
+
+		//Displaying the data from array
       for(int i =0;i<arr.length;i++) {
     	  System.out.println(arr[i]);
       }

d1e55226f5f6457db6a0ac98536898dae1bafe7f
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-07-05 08:31:48 +0530

    Create Continue.java

---
+++ Java/Week4/Day5/Continue.java
@@ -0,0 +1,18 @@
+package loops;
+
+public class ContinueInLooping {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+		
+		for(int i=5;i<=9;i++) {
+			System.out.println("KODNEST");
+			if(i==6) {
+				continue;
+			}
+			System.out.println("PVT LTD");
+		}
+	}
+
+}

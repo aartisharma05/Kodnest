7a2cdb9267e2926a006dabac4925566c9511f3a2
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-07-08 09:37:04 +0530

    Update Pattern2.java

--- Java/Week5/Day1/Patterns/Pattern2.java
+++ Java/Week5/Day1/Patterns/Pattern2.java
@@ -5,7 +5,7 @@ public class PatternProblems {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 						for(int i=1;i<=5;i++) {
-							System.out.print(" * ");
+							System.out.println(" * ");
 						}
 				scn.close();
 

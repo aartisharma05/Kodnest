00f93d29d5ac13ea3290a8f02b4880e1d155ead8
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-07-08 09:50:51 +0530

    Create Pattern9.java

---
+++ Java/Week5/Day1/Patterns/Pattern9.java
@@ -0,0 +1,17 @@
+package loops;
+
+public class Pattern8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int lines=1;lines<=5;lines++) {
+			for(int col= lines; col<=5;col++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}

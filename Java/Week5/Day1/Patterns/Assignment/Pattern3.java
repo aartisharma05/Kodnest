edb86b4a0931536a7fb9b85eeb006ee96521bf5a
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-07-09 01:22:09 +0530

    Create Pattern3.java

---
+++ Java/Week5/Day1/Patterns/Assignment/Pattern3.java
@@ -0,0 +1,16 @@
+package loops;
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int lines=1;lines<=3;lines++) {
+			for(int col= 1; col<=(2*lines-1);col++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}

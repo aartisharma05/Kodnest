47beece0d7cac103bc53073daf09821d31ad5f8b
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-07-08 09:36:44 +0530

    Create Pattern2.java

---
+++ Java/Week5/Day1/Patterns/Pattern2.java
@@ -0,0 +1,15 @@
+package loops;
+import java.util.Scanner;
+public class PatternProblems {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+						for(int i=1;i<=5;i++) {
+							System.out.print(" * ");
+						}
+				scn.close();
+
+
+	}
+
+}

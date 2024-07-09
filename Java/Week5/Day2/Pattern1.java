a2228b19f330a4aece8760d0c8d805e876f8aae3
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-07-09 08:27:33 +0530

    Create Pattern1.java

---
+++ Java/Week5/Day2/Pattern1.java
@@ -0,0 +1,34 @@
+package Patterns;
+import java.util.Scanner;
+public class Pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.println("Enter number of lines to be printed: ");
+		int lines = scn.nextInt()	;
+		System.out.println("Enter number of $ symbol to be printed: ");
+		int firstSymb = scn.nextInt()	;
+		System.out.println("Enter number of * symbol to be printed: ");
+		int secondSymb = scn.nextInt()	;
+		
+		
+		for(int i=1;i<=lines;i++) {
+			for(int j=1;j<=firstSymb;j++) {
+				System.out.print("$");
+			}
+			
+			for(int k=1;k<=secondSymb;k++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+			
+		}
+		
+		scn.close();
+	}
+
+}

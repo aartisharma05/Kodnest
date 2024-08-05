6f280e3086d9b09c648c6a01516437ce4417d146
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-08-05 12:23:43 +0530

    Create Charger.java

---
+++ Java/Week10/oops/AggregationAndComposition/MobileExample/Charger.java
@@ -0,0 +1,12 @@
+package com.kodnest.hasARelationship;
+
+public class Charger {
+ 
+	String color = "White";
+	String brand = "MI";
+	int output = 20;
+	
+	void passElectricity() {
+		System.out.println("Charger is passing the electricity and charging the device");
+	}
+}

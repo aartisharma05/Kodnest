3ffef7d7f5a2a22ad3391cdc9ab850944cd9757f
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-08-05 12:23:22 +0530

    Create OS.java

---
+++ Java/Week10/oops/AggregationAndComposition/MobileExample/OS.java
@@ -0,0 +1,13 @@
+package com.kodnest.hasARelationship;
+
+public class OS {
+
+	String name = "Android";
+	int version = 14;
+	
+    
+	
+	void makeDeviceOperational() {
+		System.out.println("OS NAME: " + name + "is making device operational.");
+	}
+}

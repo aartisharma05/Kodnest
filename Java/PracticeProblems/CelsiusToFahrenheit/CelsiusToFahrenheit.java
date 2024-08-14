1c0d407125164d9ab1eb6bc84a264eac592dfef3
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-08-14 23:55:27 +0530

    Create CelsiusToFahrenheit.java

---
+++ Java/PracticeProblems/CelsiusToFahrenheit/CelsiusToFahrenheit.java
@@ -0,0 +1,11 @@
+package practiceDay3;
+
+public class CelsiusToFahrenheit {
+   void celsiusToFahrenheit(int cel) {
+	   
+	   double fah =  ((double)9/5)* cel + 32;
+	   
+	   System.out.println("Fahrenheit = " + fah);
+	   
+   }
+}

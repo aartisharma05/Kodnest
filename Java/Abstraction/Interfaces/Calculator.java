3b3a1cce7bc40f00f957d93060047110400c420c
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-08-21 23:08:37 +0530

    Create Calculator.java

---
+++ Java/Abstraction/Interfaces/Calculator.java
@@ -0,0 +1,10 @@
+package com.kodnest.oops.interfaces;
+
+public interface Calculator {
+   public void add(int num1, int num2);
+   public void sub(int num1, int num2);
+   public void mul(int num1, int num2);
+   public void div(int num1, int num2);
+   public void mod(int num1, int num2);
+   
+}
